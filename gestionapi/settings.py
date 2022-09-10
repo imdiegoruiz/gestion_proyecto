fd8c69dc6fe0469e42fb1285171a6303474fa422
Diego F. Ruiz S <yego23@gmail.com>
2022-09-09 22:43:34 -0500

    Update from master

--- gestionapi/settings.py
+++ gestionapi/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -82,6 +83,11 @@ DATABASES = {
     }
 }
 
+
+# Heroku: Update database configuration from $DATABASE_URL.
+db_from_env = dj_database_url.config(conn_max_age=500)
+
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
